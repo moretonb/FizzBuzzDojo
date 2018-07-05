2a1bce25c3121693a4430a80476050d6b1203bc7
Moreton <moretonb@gmail.com>
2018-07-05 16:07:12 +0100

    add solution

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -6,8 +6,31 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.ReadLine();
+            for (int i = 1; i <= 100; i++)
+            {
+                var output = OutputGenerator.Generate(i);
+                Console.WriteLine(output);
+            }
+        }
+    }
+
+    public class OutputGenerator
+    {
+        public static string Generate(int number)
+        {
+            if (number % 5 == 0 && number % 3 == 0)
+            {
+                return "FizzBuzz";
+            }
+            if (number % 5 == 0)
+            {
+                return "Buzz";
+            }
+            if (number % 3 == 0)
+            {
+                return "Fizz";
+            }
+            return Convert.ToString(number);
         }
     }
 }
--- FizzBuzzDojo.UnitTests/UnitTest1.cs
+++ FizzBuzzDojo.UnitTests/UnitTest1.cs
@@ -1,18 +1,50 @@
+using FizzBuzz;
 using NUnit.Framework;
 
 namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(44, "44")]
+        [TestCase(98, "98")]
+        public void WhenPassingANumberToOutputGeneratorTheNumberAsAStringIsReturned(int input, string expectedOutput)
         {
+            var output = OutputGenerator.Generate(input);
+            Assert.That(output, Is.EqualTo(expectedOutput));
         }
 
-        [Test]
-        public void Test1()
+        [TestCase(3)]
+        [TestCase(9)]
+        [TestCase(99)]
+        public void WhenPassingANumberDivisibleByThreeToOutputGeneratorFizzIsReturned(int input)
         {
-            Assert.Pass();
+            const string expectedOutput = "Fizz";
+            var output = OutputGenerator.Generate(input);
+
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
+        [TestCase(5)]
+        [TestCase(100)]
+        public void WhenPassingANumberDivisibleByFiveToOutputGeneratorBuzzIsReturned(int input)
+        {
+            const string expectedOutput = "Buzz";
+            var output = OutputGenerator.Generate(input);
+
+            Assert.That(output, Is.EqualTo(expectedOutput));
+        }
+
+        [TestCase(0)]
+        [TestCase(15)]
+        [TestCase(90)]
+        public void WhenPassingANumberDivisibleByThreeAndFiveToOutputGeneratorFizzBuzzIsReturned(int input)
+        {
+            const string expectedOutput = "FizzBuzz";
+            var output = OutputGenerator.Generate(input);
+
+            Assert.That(output, Is.EqualTo(expectedOutput));
         }
     }
 }
